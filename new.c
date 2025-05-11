8de3283bece6f3744ff5f41462502b29cfc584b9
THEJAS-BK <thejasbk998@gmail.com>
2025-05-11 10:22:52 +0530

    C Todo list

---
+++ new.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+char tasks[25][25];
+void printTable(int n)
+{
+    int i, length = 0;
+    length += n;
+    printf("\nindex\t\tTasks \n");
+    for (i = 0; i < length; i++)
+    {
+        if (tasks[i][0] != '\0')
+            printf("%d\t\t%s \n", i + 1, tasks[i]);
+    }
+}
+int ask_user()
+{
+    int userinp, i;
+    while (1)
+    {
+        printf("enter the number of tasks to add (max 20) :");
+        scanf("%d", &userinp);
+        getchar();
+        if (userinp <= 20 && userinp > 0)
+        {
+            printf("\n");
+            for (i = 3; i < (userinp + 3); i++)
+            {
+                printf("enter task %d :", i + 1);
+                fgets(tasks[i], sizeof(tasks[i]), stdin);
+                if (tasks[i][strlen(tasks[i]) - 1] == '\n')
+                {
+                    tasks[i][strlen(tasks[i]) - 1] = '\0';
+                }
+            }
+            printTable(userinp + 3);
+            break;
+        }
+        }
+    return userinp + 3;
+}
+int Edit_mode(int n)
+{
+    int idx;
+    while (1)
+    {
+        printf("enter the index of the task to delete :");
+        scanf("%d", &idx);
+        if (idx > 0 && idx < n)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid index");
+        }
+    }
+
+    strcpy(tasks[idx - 1], "");
+    for (int i = idx - 1; i < n - 1; i++)
+    {
+        strcpy(tasks[i], tasks[i + 1]);
+    }
+    tasks[n - 1][0], '\0';
+    return n - 1;
+}
+
+void main()
+{
+    char user_inp;
+    int i, n = 3, val, val2;
+    printf("\n       TODO LIST \n\n");
+    printf("Let's plan your day (c to create)");
+    scanf("%c", &user_inp);
+    getchar();
+    if ((toupper(user_inp)) == 'C')
+    {
+        for (i = 0; i < n; i++)
+        {
+            printf("enter tasks %d :", i + 1);
+            fgets(tasks[i], sizeof(tasks[i]), stdin);
+            if (tasks[i][strlen(tasks[i]) - 1] == '\n')
+            {
+                tasks[i][strlen(tasks[i]) - 1] = '\0';
+            }
+        }
+        printf("\n");
+        printTable(n);
+        printf("\n");
+        val = ask_user();
+        val2 = Edit_mode(val);
+        printTable(val2);
+    }
+}
